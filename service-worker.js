f75ddac6e2d9b4a96992a22080309cd0ce2b8ee1
poorlilgoblin <dagmara2003@gmail.com>
2025-04-24 20:17:53 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('calendar-cache').then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ])
+    )
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
